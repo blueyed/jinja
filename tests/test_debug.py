d767d27f12710ddb332731aa6eacdfffdb5801d6
David Lord <davidism@gmail.com>
2019-12-06 10:29:06 -0800

    rename imports to jinja

--- tests/test_debug.py
+++ tests/test_debug.py
@@ -1,22 +1,13 @@
 # -*- coding: utf-8 -*-
-"""
-    jinja2.testsuite.debug
-    ~~~~~~~~~~~~~~~~~~~~~~
-
-    Tests the debug system.
-
-    :copyright: (c) 2017 by the Jinja Team.
-    :license: BSD, see LICENSE for more details.
-"""
 import pytest
 
 import re
 import sys
 from traceback import format_exception
 
-from jinja2 import ChoiceLoader
-from jinja2 import DictLoader
-from jinja2 import Environment, TemplateSyntaxError
+from jinja import ChoiceLoader
+from jinja import DictLoader
+from jinja import Environment, TemplateSyntaxError
 
 
 @pytest.fixture
@@ -59,7 +50,7 @@ ZeroDivisionError: (int(eger)? )?division (or modulo )?by zero
         self.assert_traceback_matches(lambda: fs_env.get_template('syntaxerror.html'), r'''(?sm)
   File ".*?syntaxerror.html", line 4, in (template|<module>)
     \{% endif %\}.*?
-(jinja2\.exceptions\.)?TemplateSyntaxError: Encountered unknown tag 'endif'. Jinja was looking for the following tags: 'endfor' or 'else'. The innermost block that needs to be closed is 'for'.
+(jinja\.exceptions\.)?TemplateSyntaxError: Encountered unknown tag 'endif'. Jinja was looking for the following tags: 'endfor' or 'else'. The innermost block that needs to be closed is 'for'.
     ''')
 
     def test_regular_syntax_error(self, fs_env):
@@ -68,7 +59,7 @@ ZeroDivisionError: (int(eger)? )?division (or modulo )?by zero
         self.assert_traceback_matches(test, r'''
   File ".*debug.pyc?", line \d+, in test
     raise TemplateSyntaxError\('wtf', 42\)
-(jinja2\.exceptions\.)?TemplateSyntaxError: wtf
+(jinja\.exceptions\.)?TemplateSyntaxError: wtf
   line 42''')
 
     def test_include_syntax_error_source(self, filesystem_loader):
@@ -86,8 +77,8 @@ ZeroDivisionError: (int(eger)? )?division (or modulo )?by zero
         assert exc_info.value.source is not None
 
     def test_local_extraction(self):
-        from jinja2.debug import get_template_locals
-        from jinja2.runtime import missing
+        from jinja.debug import get_template_locals
+        from jinja.runtime import missing
         locals = get_template_locals({
             'l_0_foo': 42,
             'l_1_foo': 23,
